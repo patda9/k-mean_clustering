582f05b999c9c084dc2c54beebf300f668c81464
patdanai.duangwaropas@gmail.com <patdanai.duangwaropas@gmail.com>
2018-04-26 23:01:57 +0700

    add purity calculation after finish partitioning

--- k_mean_clustering.py
+++ k_mean_clustering.py
@@ -34,98 +34,148 @@ def update_centroid(p, cents, eu_d):
 
     return [np.array(clusters), cents_diff, sum_cents_diff]
 
+def write_output(k, i, output, e = False, mse = False, clusters = False):
+    if(e == True):
+        np.savetxt('./log/mse_iteration' + str(i) + '.csv', output, delimiter = ',', fmt = '%0.6f')
+    elif(mse == True):
+        output = np.array([output])
+        np.savetxt('./log/sum_mse_itration' + str(i) + '.csv', output, delimiter = ',', fmt = '%0.6f')
+    elif(clusters == True):
+        for j in range(0, len(output)):
+            np.savetxt('./log/clusters' + str(j) + '_iteration' + str(i) + '.csv', output[j], delimiter = ',', fmt = '%0.1f')
+    
+    output = np.array(output)
+    for j in range(0, len(output)):
+        np.savetxt('./log/centroids' + str(j) + '_iteration' + str(i) + '.csv', output, delimiter = ',', fmt = '%0.6f')
+
+def purity(clusters):
+    n = 0
+    purity = 0.
+    max_f = []
+    
+    for i in range(0, len(clusters)):
+        temp = np.transpose(clusters[i])
+        for j in range(0, len(temp)):
+            if(i < 1):
+                counts = collections.Counter(temp[j])
+                max_f_in_a = list(counts.values())[0]
+                n += sum(list(counts.values()))
+                max_f.append(max_f_in_a)
+            else:
+                counts = collections.Counter(temp[j])
+                max_f_in_a = list(counts.values())[0]
+                n += sum(list(counts.values()))
+                if(max_f_in_a > max_f[j]):
+                    max_f[j] = max_f_in_a
+
+    purity = np.sum(max_f) * 100 / n
+
+    print('Purity:', purity, '%')
+    return purity
+
+        
+
+np.random.seed(1)
 np.set_printoptions(suppress = True)
 
-E = np.inf
+sum_mse = np.inf
 k = 4
 
-## Data mining course dataset ##
-# data_set = data_map(preprocess('./dresses_attribute_sales.csv'))
-## Data mining course dataset ##
-
-test_data_set = np.array([[185, 72], [170, 56], [168, 60], [179, 68], [182, 72], [188, 77]], dtype = np.float32)
-data_set = np.random.randint(200, size = (3200, 2))
+data_set = data_map(preprocess('./dresses_attribute_sales.csv')) # Data mining course dataset
+# ex_data_set = np.array([[185, 72], [170, 56], [168, 60], [179, 68], [182, 72], [188, 77]], dtype = np.float32)
+# test_data_set = np.random.randint(200, size = (3200, 2)) # test dataset
 
 centroids = np.zeros((k, len(data_set[0]))) # random pick centroids k = 2
 
 initialize_centroids(centroids, data_set)
 
 i = 0
-while(E > 0.0000001):
+while(sum_mse > 0.0000001):
     print('Itteration:', i)
     euclidian_distances = euclidian_distance(data_set, centroids, k)
     updates = update_centroid(data_set, centroids, euclidian_distances)
     clusters = updates[0]
-    E = updates[2]
+    sum_mse = updates[2]
     print('Updated Centroids:')
     print(centroids)
-    print('e_sqr:', updates[1])
-    print('E_sqr:' , E)
-    print('At index', i, 'Clusters:')
-    print(clusters)
+    write_output(k, i, centroids)
+
+    # print('MSE:', updates[1])
+    # write_output(k, i, updates[1], e = True)
+
+    # print('Sum MSE:' , sum_mse)
+    # write_output(k, i, sum_mse, mse = True)
+
+    # print('At index', i, 'Clusters:')
+    # print(clusters)
+    # write_output(k, i, clusters, clusters = True)
+
     i += 1
 
-for i in range(0, len(clusters)):
-    clusters[i] = np.transpose(clusters[i])
-    print(clusters[i])
-centroids = centroids.T
+purity(clusters)
 
-trace1 = go.Scatter(
-    x = clusters[0][0],
-    y = clusters[0][1],
-    name = 'Cluster1',
-    mode = 'markers',
-    marker = dict(
-        size = 8,
-        color = '#33ffdd')
-)
+def plotting_test(centroids, clusters):
+    for i in range(0, len(clusters)):
+        clusters[i] = np.transpose(clusters[i])
+        print(clusters[i])
+    centroids = centroids.T
 
-trace2 = go.Scatter(
-    x = clusters[1][0],
-    y = clusters[1][1],
-    name = 'Cluster2',
-    mode = 'markers',
-    marker = dict(
-        size = 8,
-        color = '#32ff23')
-)
-
-trace3 = go.Scatter(
-    x = clusters[2][0],
-    y = clusters[2][1],
-    name = 'Cluster3',
-    mode = 'markers',
-    marker = dict(
-        size = 8,
-        color = '#ffdd11')
-)
-
-trace4 = go.Scatter(
-    x = clusters[3][0],
-    y = clusters[3][1],
-    name = 'Cluster4',
-    mode = 'markers',
-    marker = dict(
-        size = 8,
-        color = '#ff2222')
-)
-
-trace5 = go.Scatter(
-    x = centroids[0],
-    y = centroids[1],
-    name = 'Centroid',
-    mode = 'markers',
-    marker = dict(
-    size = 16,
-    color = '#000000',
+    trace1 = go.Scatter(
+        x = clusters[0][0],
+        y = clusters[0][1],
+        name = 'Cluster1',
+        mode = 'markers',
+        marker = dict(
+            size = 8,
+            color = '#33ffdd')
     )
-)
 
-layout = go.Layout(
-    title= '4-Mean Clustering',
-    hovermode= 'closest'
-)
+    trace2 = go.Scatter(
+        x = clusters[1][0],
+        y = clusters[1][1],
+        name = 'Cluster2',
+        mode = 'markers',
+        marker = dict(
+            size = 8,
+            color = '#32ff23')
+    )
 
-data = [trace1, trace2, trace3, trace4, trace5]
-fig= go.Figure(data=data, layout=layout)
-py.offline.plot(fig, filename = 'example.html')
+    trace3 = go.Scatter(
+        x = clusters[2][0],
+        y = clusters[2][1],
+        name = 'Cluster3',
+        mode = 'markers',
+        marker = dict(
+            size = 8,
+            color = '#ffdd11')
+    )
+
+    trace4 = go.Scatter(
+        x = clusters[3][0],
+        y = clusters[3][1],
+        name = 'Cluster4',
+        mode = 'markers',
+        marker = dict(
+            size = 8,
+            color = '#ff2222')
+    )
+
+    trace5 = go.Scatter(
+        x = centroids[0],
+        y = centroids[1],
+        name = 'Centroid',
+        mode = 'markers',
+        marker = dict(
+        size = 16,
+        color = '#000000',
+        )
+    )
+
+    layout = go.Layout(
+        title= '4-Mean Clustering',
+        hovermode= 'closest'
+    )
+
+    data = [trace1, trace2, trace3, trace4, trace5]
+    fig= go.Figure(data=data, layout=layout)
+    py.offline.plot(fig, filename = 'example.html')
